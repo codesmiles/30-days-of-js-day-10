616e7b506973a6988cbb64f3a43eeb5c2cf40757
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-04-07 23:56:51 +0100

    update

--- levels.js
+++ levels.js
@@ -1,22 +1,25 @@
 // create an empty set
 const empty = new Set();
-console.log(empty);
+// console.log(empty);
 
 // Create a set containing 0 to 10 using loop
 const numArr = new Set();
 for (i = 0; i <= 10; i++) {
   numArr.add(i);
 }
-console.log(numArr);
+// console.log(numArr);
+
 // Remove an element from a set
 numArr.delete(Math.round(Math.random() * 10));
-console.log(numArr);
+// console.log(numArr);
+
 // Clear a set
 numArr.clear();
 // Create a set of 5 string elements from array
 const str = ["a", "b", "c", "d", "e"];
 const fiveStrings = new Set(str);
-console.log(fiveStrings);
+// console.log(fiveStrings);
+
 // Create a map of countries and number of characters of a country
 const countries = ["Finland", "Sweden", "Norway"];
 const countriesAndNumberMap = new Map();
@@ -32,11 +35,20 @@ countries.forEach((el) => {
 console.log(countriesAndNumberMap);
 const a = [ 4, 5, 8, 9];
 const b = [3, 4, 5, 7];
+
 // Exercises:Level 2
 // Find a union b
 const c = new Set([...a, ...b]);//or basicaly store them in a variable;
-console.log(c);
+// console.log(c);
+
 // Find a intersection b
 
+let A = new Set(a);
+let B = new Set(b);
+
+let interset = a.filter((num) => B.has(num));
+
+console.log(interset);
 
 // Find a with b
+
