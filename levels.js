4666d00214e54b2c6e0ca895fd20f3273b937034
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-04-08 00:06:17 +0100

    update

--- levels.js
+++ levels.js
@@ -23,16 +23,17 @@ const fiveStrings = new Set(str);
 // Create a map of countries and number of characters of a country
 const countries = ["Finland", "Sweden", "Norway"];
 const countriesAndNumberMap = new Map();
-countries.forEach((el) => {
+countries.map((el) => {
   countriesAndNumberMap.set(el, el.length);
 });
+// console.log(countriesAndNumberMap);
 
 // ALTERNATIVELY
 // for (i = 0; i < countries.length; i++){
 //     countriesAndNumberMap.set(countries[i], countries[i].length);
 // }
+// console.log(countriesAndNumberMap);
 
-console.log(countriesAndNumberMap);
 const a = [ 4, 5, 8, 9];
 const b = [3, 4, 5, 7];
 
@@ -52,3 +53,27 @@ console.log(interset);
 
 // Find a with b
 
+// Exercises:Level 3
+// How many languages are there in the countries object file.
+
+
+// *** Use the countries data to find the 10 most spoken languages:
+
+// // Your output should look like this
+// console.log(mostSpokenLanguages(countries, 10))[
+//   ({ English: 91 },
+//   { French: 45 },
+//   { Arabic: 25 },
+//   { Spanish: 24 },
+//   { Russian: 9 },
+//   { Portuguese: 9 },
+//   { Dutch: 8 },
+//   { German: 7 },
+//   { Chinese: 5 },
+//   { Swahili: 4 },
+//   { Serbian: 4 })
+// ]
+
+// // Your output should look like this
+// console.log(mostSpokenLanguages(countries, 3))
+// [ {'English':91}, {'French':45}, {'Arabic':25} ]
